d38abb2b863fa16518238efe74f9aa1fc90f24e2
Alex Kurzhanskiy <akurzhan@gmail.com>
2016-07-10 17:42:57 -0700

    Headway for IDM equilibria

--- car_following_model/idm_equilibrium.py
+++ car_following_model/idm_equilibrium.py
@@ -23,22 +23,31 @@ import matplotlib.pyplot as plt
 def main(argv):
     print(__doc__)
     
+    l = 5 # meters
     g_min = 4 # meters
     v_max = 20 # m/s
     tau = 2.05 # seconds
     delta = 4 # acceleration exponent 
     
+    dv = 0.1
+    
 
     speed = []
     gap = []
+    headway = []
     
     v = 0
     while v < v_max:
         g = (g_min + v*tau) / np.sqrt(1 - float(v/v_max)**delta)
         speed.append(v)
         gap.append(g)
-        print(g, v)
-        v += 0.1
+        if v < dv:
+            headway.append(tau)
+        else:
+            headway.append((g+l)/v)
+            
+        print(g, v, headway[-1])
+        v += dv
     
     
     if True:
@@ -47,9 +56,15 @@ def main(argv):
         plt.plot(gap, speed, 'o')
         plt.xlabel('Gap')
         plt.ylabel('Speed')
+        
+        plt.figure()
+        plt.plot(speed, headway, 'k')
+        plt.plot(speed, headway, 'o')
+        plt.xlabel('Speed')
+        plt.ylabel('Headway')
         plt.show()
-    
-    
+
+
 
 
 
