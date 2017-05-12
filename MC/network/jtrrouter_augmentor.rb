d3fc51cb3b7a15bc0805e4d14ec1cff863097b55
Alex Kurzhanskiy <akurzhan@gmail.com>
2017-05-11 21:47:58 -0700

    Route augmentor: model specifications in car types

--- MC/network/jtrrouter_augmentor.rb
+++ MC/network/jtrrouter_augmentor.rb
@@ -29,10 +29,10 @@ f.each_line do |line|
     fo.write(line) # Set car definitions at the start
   
     #fo.write('    <vType id="CarA" length="5.0" color="0,1,0" accel="1.5" decel="2" tau="1.1" sigma="0.0" impatience="0.0" minGap="0.5" maxSpeed="70.0"/>')
-    fo.write('    <vType id="CarA" carFollowModel="IIDM" length="5.0" color="0,1,0" accel="1.5" decel="2" tau="1.1" sigma="0.0" impatience="0.0" minGap="0.5" maxSpeed="70.0"/>')
+    fo.write('    <vType id="CarA" carFollowModel="IIDM" laneChangeModel="LC2013" length="5.0" color="0,1,0" accel="1.5" decel="2" tau="1.1" sigma="0.0" impatience="0.0" minGap="3.0" maxSpeed="70.0"/>')
     fo.write("\n")
     #fo.write('    <vType id="CarM" length="5.0" color="1,1,0" accel="1.5" decel="2" tau="2.05" sigma="0.0" impatience="0.0" minGap="2.0" maxSpeed="70.0"/>\n')
-    fo.write('    <vType id="CarM" carFollowModel="IIDM" length="5.0" color="0,0,1" accel="1.5" decel="2" tau="2.05" sigma="0.0" impatience="0.0" minGap="2.0" maxSpeed="70.0"/>')
+    fo.write('    <vType id="CarM" carFollowModel="IIDM" laneChangeModel="LC2013" length="5.0" color="0,0,1" accel="1.5" decel="2" tau="2.05" sigma="0.0" impatience="0.0" minGap="4.0" maxSpeed="70.0"/>')
     fo.write("\n")
   elsif line =~  /id="(.*)"/
     #<vehicle depart="0.0" id="veh1" route="1" type="CarM" departSpeed="max"/>
